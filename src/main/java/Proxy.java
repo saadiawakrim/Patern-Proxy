81ee10b30f5e19e69c4169ccc49c4f1464a944cc
saadiawak98 <saadiawak98@gmail.com>
2024-01-22 10:47:59 +0100

    first commit

---
+++ src/main/java/Proxy.java
@@ -0,0 +1,18 @@
+
+import java.util.Random;
+
+public class Proxy implements Standard{
+
+    private Standard standard;
+    @Override
+    public void process() {
+        boolean access = new Random().nextBoolean();
+        if(access){
+            standard = new StandardImpl1();
+            standard.process();
+        }
+        else
+            throw new RuntimeException("Forbidden 403");
+    }
+
+}
---
+++ src/main/java/StandardImpl1.java
@@ -0,0 +1,8 @@
+public class StandardImpl1 implements Standard{
+    @Override
+    public void process() {
+        System.out.println("***********************");
+        System.out.println("Process StandardIml 1");
+        System.out.println("***********************");
+    }
+}
